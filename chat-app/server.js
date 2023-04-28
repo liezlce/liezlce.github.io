eb84627b35e624c1ea212839a9f5caaf9bc1639e
abigaelluetaa <100799241+abigaelluetaa@users.noreply.github.com>
2023-04-28 15:30:20 -0700

    integrated messageDatabase.js to server.js

--- chat-app/server.js
+++ chat-app/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const { auth, requiresAuth } = require('express-openid-connect');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+
 const app = express();
 
 const config = {
@@ -26,6 +30,37 @@ app.get('/', (req, res) => {
 app.get('/profile', requiresAuth(), (req, res) => {
   res.send(JSON.stringify(req.oidc.user, null, 2));
 });
+
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+async function connect() {
+  await mongoose.connect("mongodb+srv://abigaelluetaa:<y4eXjFND-yzs!pK>@cluster0.qwy4fob.mongodb.net/test") // MAYBE CHANGE THIS TO MY OWN?????
+}
+
+const messageSchema = new mongoose.schema({
+  to: String,
+  content: String,
+  from: String,
+  time: Date
+})
+
+const Message = mongoose.model('Message', messageSchema)
+
+app.get("/chat/:from/:to", async (req, res) => {
+  const foundMes = await Message.find({$or: [{$and: [{to: req.params.to, from:req.params.from}, {to:req.params.from, from:req.params.to}]}]});
+  return res.send(foundMes)
+})
+
+app.post("/chat/send/:from/:to", async (req, res) => {
+  var today = new Date();
+  const newMessage = new Message({to:req.body.to, content:req.body.content, from:req.body.from, time:today})
+  return res.send(newMessage)
+})
+
+connect().catch(err => console.log(err))
 
 app.listen(3000, function() {
   console.log('Listening on http://localhost:3000');
