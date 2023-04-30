dcd83ba724f1238a27d9601a84744df06e0c4253
abigaelluetaa <100799241+abigaelluetaa@users.noreply.github.com>
2023-04-30 14:42:07 -0700

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-=======
 import './App.css';
 import { useState } from 'react'; 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -72,4 +45,3 @@ function App() {
 }
 
 export default App;
->>>>>>> origin/bryan_branch
